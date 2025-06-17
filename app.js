111ef8a017a60664599fea606a406e91adb55b00
Ryan Ferreira <RyanMatheusFerreira@outlook.com.br>
2025-06-16 23:33:47 -0300

    Login e SignUp funcionais

--- app.js
+++ app.js
@@ -1,41 +1,44 @@
-import path from 'path';
-
-const PORT = 3000;
-
 import express from 'express';
 import bodyParser from 'body-parser';
+import { sequelize } from './models/database.js';
 
-import { commentsRoute } from './routes/comments.js';
 import { usersRoute } from './routes/users.js';
+import { commentsRoute } from './routes/comments.js';
 import { postsRoute } from './routes/post.js';
 import { authRoute } from './routes/auth.js';
 
-import { sequelize } from './models/database.js';
+import { swaggerSpec, swaggerUi } from './swagger.js';
 
 export const app = express();
 
-app.use(bodyParser.json());
+const PORT = 3000;
 
-app.use(commentsRoute);
+/* Vou começar a usar EJS como template engine, aparentemente é um dos mais utilizados.
+Tem uma boa documentação por aí. */
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // Para ler dados de forms
+
 app.use(usersRoute);
+app.use(commentsRoute);
 app.use(postsRoute);
 app.use(authRoute);
 
-app.use('/static', express.static('public'));
-
-import { swaggerSpec, swaggerUi } from './swagger.js';
-
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.get('/signup', (req, res) => { res.render('signup'); });
+app.get('/login', (req, res) => { res.render('login'); });
 
 export async function App() {
     await sequelize.sync();
-
-    const server = app.listen(PORT, () => {
+    app.listen(PORT, () => {
         console.log(`Servidor rodando na porta ${PORT}`);
-
-        console.log(`Documentação visual e interativa das rotas e requisições disponível em http://localhost:3000/api-docs`);
+        console.log(`Acesse http://localhost:3000/signup`);
     });
-    return server;
 }
 
 App();
